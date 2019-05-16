95a1d9aee18a358d1ef4a3055323954e3e9ff36b
Usama Saleem <usamasaleem674@gmail.com>
2019-05-16 22:46:22 +0500

    Dating App

---
+++ app/src/main/java/com/jackoftech/androidproject/Login.java
@@ -0,0 +1,82 @@
+package com.jackoftech.androidproject;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    private EditText rEmail, rPass;
+    private Button nLogin;
+
+
+    private FirebaseAuth rAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        rAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (fUser != null){
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        nLogin = findViewById(R.id.btnLogin);
+
+        rEmail = findViewById(R.id.email);
+        rPass = findViewById(R.id.pass);
+
+        nLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = rEmail.getText().toString();
+                final String pass = rPass.getText().toString();
+
+                rAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        rAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        rAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+}
---
+++ app/src/main/java/com/jackoftech/androidproject/LoginOrRegister.java
@@ -0,0 +1,45 @@
+package com.jackoftech.androidproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginOrRegister extends AppCompatActivity {
+
+    private Button btnLogin , btnRegister ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_register);
+
+
+        btnLogin = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.register);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrRegister.this, Login.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrRegister.this, Registration.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
